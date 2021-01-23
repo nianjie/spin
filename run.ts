0caf40295f411b5f4a572f4fa1db6cdfb1b54a4e
nj <4769034+nianjie@users.noreply.github.com>
2021-01-23 10:17:28 +0900

    improve platform independency.

--- run.ts
+++ run.ts
@@ -6,6 +6,7 @@ import { existsSync } from "fs"
 import { execSync } from "child_process"
 import * as gm from "gm"
 import { exit } from "process"
+import * as rimraf from 'rimraf'
 
 (async () => {
 
@@ -38,35 +39,36 @@ import { exit } from "process"
     }
   );
   console.log(`Got PRs for ${response.viewer.login}`)
-  const prs = response.viewer.pullRequests.nodes
+  rimraf.sync(`./images/*.png`)
   
+  const prs = response.viewer.pullRequests.nodes
   const browser = await playwright["chromium"].launch()
-    for (const pr of prs) {
-      const i = prs.indexOf(pr)
-      const context = await browser.newContext()
-      const page = await context.newPage()
-    
-      const url = `file:${join(__dirname, 'web', 'index.html')}`
-      await page.goto(url);
-    
-      const JS = `
-      document.getElementById("org-icon").src = '${pr.repository.owner.avatarUrl}'
-      document.getElementById("repo-name").innerText = '${pr.repository.owner.login}/${pr.repository.name}'
-      document.getElementById("pr-name").innerText = '${pr.title.replace(/`/g, "'")}'
-      document.getElementById("pr-body").innerHTML = '${pr.bodyHTML.replace(/`/g, "'")}'
-      document.getElementById("status").style.display = '${pr.state === "MERGED" ? "block" : "none"}'
+  for (const pr of prs) {
+    const i = prs.indexOf(pr)
+    const context = await browser.newContext()
+    const page = await context.newPage()
+  
+    const url = `file:${join(__dirname, 'web', 'index.html')}`
+    await page.goto(url);
+  
+    const JS = `
+    document.getElementById("org-icon").src = '${pr.repository.owner.avatarUrl}'
+    document.getElementById("repo-name").innerText = '${pr.repository.owner.login}/${pr.repository.name}'
+    document.getElementById("pr-name").innerText = '${pr.title.replace(/`/g, "'")}'
+    document.getElementById("pr-body").innerHTML = '${pr.bodyHTML.replace(/`/g, "'")}'
+    document.getElementById("status").style.display = '${pr.state === "MERGED" ? "block" : "none"}'
 
-      setIndex(${i}, ${prs.length})
-      `
-      try {
-        await page.evaluate(JS);
-        await page.screenshot({ path: `./images/${i}.png`, clip: { x: 0, y: 0, width: 378, height: 100 } });
-      } catch (error) {
-        console.error(error)
-        console.log(JS)
-        await browser.close()
-      }
+    setIndex(${i}, ${prs.length})
+    `
+    try {
+      await page.evaluate(JS);
+      await page.screenshot({ path: `./images/${i}.png`, clip: { x: 0, y: 0, width: 378, height: 100 } });
+    } catch (error) {
+      console.error(error)
+      console.log(JS)
+      await browser.close()
     }
+  }
 
   console.log("made screenshots")
   await browser.close();
@@ -90,7 +92,7 @@ import { exit } from "process"
   }
 
   if (existsSync(gistProps.name)) {
-    execSync(`rm -rf ${gistProps.name}`)
+    rimraf.sync(`${gistProps.name}`)
   }
   execSync(`git clone https://${response.viewer.login}:${process.env.GITHUB_API_TOKEN}@gist.github.com/${gistProps.name}.git`)
 
